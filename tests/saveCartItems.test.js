44d7460bf72b47a11ad2e774cf5cbaf15cbd7982
Ikarosv <ikarovieiraa@gmail.com>
2022-10-11 18:57:24 -0300

    Implementado o teste de saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,13 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se ao executar a função o localStorage.setItem é chamado.', () => {
+    saveCartItems('cartItem');
+    expect(global.localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Testa se ao executar a função o localStorage.setItem é chamado com dois parâmetros.', () => {
+    saveCartItems('cartItem');
+    expect(global.localStorage.setItem).toHaveBeenCalledWith('cartItems', 'cartItem');
+  });
 });
