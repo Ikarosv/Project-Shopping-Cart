c0cccffe93745b0d8b429b593c291799650fab74
Ikarosv <ikarovieiraa@gmail.com>
2022-10-11 17:12:36 -0300

    Testes de fetchItem implementados

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,18 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchItem é uma função.', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Testa se fetch foi chamado.', async () => {
+    await fetchItem('MLB1615760527');
+    expect(global.fetch).toHaveBeenCalled();
+  });
+
+  it('Testa se a função fetchItem utiliza o endpoint correto.', async () => {
+    await fetchItem('MLB1615760527');
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527' 
+    expect(global.fetch).toHaveBeenCalledWith(endpoint);
+  });
 });
