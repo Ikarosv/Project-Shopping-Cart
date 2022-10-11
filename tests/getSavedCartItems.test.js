c8a98deea50eb0003a16e7f8c8e6d5e1d3251382
Ikarosv <ikarovieiraa@gmail.com>
2022-10-11 19:02:54 -0300

    Testes de getSavedCartItems implementados

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,13 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se ao executar a função o método localStorage.getItem é chamado', () => {
+    getSavedCartItems();
+    expect(global.localStorage.getItem).toHaveBeenCalled();
+  });
+
+  it('Teste se ao executar a função o método localStorage.getItem é chamado com um parametro', () => {
+    getSavedCartItems()
+    
+  });
 });
