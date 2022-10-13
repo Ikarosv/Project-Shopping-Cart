5de04c806346af293303c52701d1b896e30bfefd
Ikarosv <ikarovieiraa@gmail.com>
2022-10-13 20:44:29 -0300

    Refatora saveCartItems

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,4 @@
-const saveCartItems = (cartItem) => {
-  localStorage.setItem('cartItems', cartItem);
-};
+const saveCartItems = (cartItem) => localStorage.setItem('cartItems', cartItem);
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
