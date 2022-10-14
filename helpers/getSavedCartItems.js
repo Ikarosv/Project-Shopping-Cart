3d886210fe3f4b25769c1ab4e5948b31a8ab83ba
Ikarosv <ikarovieiraa@gmail.com>
2022-10-13 21:42:35 -0300

    refatora getSavedCartItems e seus testes

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,4 +1,9 @@
-const getSavedCartItems = () => JSON.parse(localStorage.getItem('cartItems'));
+const getSavedCartItems = () => {
+  const cartItemsLocalStorage = localStorage.getItem('cartItems');
+  if (cartItemsLocalStorage) {
+    return JSON.parse(cartItemsLocalStorage);
+  }
+};
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -6,11 +6,11 @@ localStorageSimulator('getItem');
 describe('4 - Teste a função getSavedCartItems', () => {
   it('Testa se ao executar a função o método localStorage.getItem é chamado', () => {
     getSavedCartItems();
-    expect(global.localStorage.getItem).toHaveBeenCalled();
+    expect(localStorage.getItem).toHaveBeenCalled();
   });
 
   it('Teste se ao executar a função o método localStorage.getItem é chamado com um parametro', () => {
     getSavedCartItems();
-    expect(global.localStorage.getItem).toHaveBeenCalledWith('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });
 });
