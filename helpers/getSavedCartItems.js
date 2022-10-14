571ebdf4d352ab2bde79272719bd94f3c5557263
Ikarosv <ikarovieiraa@gmail.com>
2022-10-13 22:07:19 -0300

    refatora getSavedCartItem e seu teste

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,8 +1,9 @@
 const getSavedCartItems = () => {
   const cartItemsLocalStorage = localStorage.getItem('cartItems');
-  if (cartItemsLocalStorage) {
-    return JSON.parse(cartItemsLocalStorage);
-  }
+  // if (cartItemsLocalStorage) {
+  //     return JSON.parse(cartItemsLocalStorage);
+  // }
+  return cartItemsLocalStorage ? JSON.parse(cartItemsLocalStorage) : cartItemsLocalStorage;
 };
 
 if (typeof module !== 'undefined') {
--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -8,9 +8,19 @@ describe('4 - Teste a função getSavedCartItems', () => {
     getSavedCartItems();
     expect(localStorage.getItem).toHaveBeenCalled();
   });
-
+  
   it('Teste se ao executar a função o método localStorage.getItem é chamado com um parametro', () => {
     getSavedCartItems();
     expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });
+  
+  // it('Testa se retorna o esperado', () => {
+
+  //   // localStorageSimulator('setItem');
+  //   JSON.parse = jest.fn().mockImplementationOnce(() => {
+  //     // return your what your code is returning.
+  //   });
+  //   getSavedCartItems();
+  //   expect(JSON.parse).toHaveBeenCalled();
+  // });
 });
